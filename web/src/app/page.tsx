33bc4765281e6ff40bffe2d1e067edad97e0e620
fcintra <matheus.cintra_2002@hotmail.com>
2023-05-16 18:37:33 -0300

    web and mobile

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,9 @@
-
 export default function Home() {
-  return (  
-    <h1>Hello World</h1>
+  return (
+    <>
+      <div className="h-screen bg-zinc-900 p-6 text-zinc-50">
+        <h1 className="text-4xl font-bold">Sua capsula do tempo</h1>
+      </div>
+    </>
   )
 }
