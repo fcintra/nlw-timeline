05b92ce5cbea91d116854c25a7af5a51087a6a84
fcintra <matheus.cintra_2002@hotmail.com>
2023-05-17 22:53:26 -0300

    aula3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,12 @@
-import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
 import fastify from 'fastify'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
-
-app.get('/hello', async () => {
-  const users = await prisma.user.findMany()
-  return users
+app.register(cors, {
+  origin: ['http://localhost:3000'], // lista de URLs que podem acessar o backend
 })
+app.register(memoriesRoutes)
 
 app
   .listen({
